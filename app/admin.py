a8ac62e31523fdac86e9f8ec97f8ab89ccdc9e61
ivanleoncz <ivanlmj@gmail.com>
2026-08-04 13:11:00 -0600

    feat: django admin UI improvements
    Models and their relations.

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,53 @@ from django.contrib import admin
 
 from .models import YearModel, MovieModel, StudioModel, ProducerModel
 
-admin.site.register(YearModel)
-admin.site.register(MovieModel)
-admin.site.register(StudioModel)
-admin.site.register(ProducerModel)
+
+class MovieInLine(admin.TabularInline):
+    model = MovieModel
+    extra = 1
+
+@admin.register(YearModel)
+class YearAdmin(admin.ModelAdmin):
+    inlines = [MovieInLine, ]
+    list_display = ('year', 'get_movies')
+    ordering = ('year', )
+
+    def get_movies(self, obj):
+        return ", ".join([movie.title for movie in obj.movies.all()])
+
+    get_movies.short_description = "movies"
+
+@admin.register(MovieModel)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'year', 'get_studios', 'get_producers')
+    search_fields = ('title', )
+    ordering = ('title', )
+
+    def get_studios(self, obj):
+        return ', '.join([studio.name for studio in obj.studios.all()])
+
+    def get_producers(self, obj):
+        return ', '.join([producer.name for producer in obj.producers.all()])
+
+    get_studios.short_description = "Studios"
+    get_producers.short_description = "Producers"
+
+@admin.register(StudioModel)
+class StudioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_movies')
+    search_fields = ('name', )
+    ordering = ('name', )
+    def get_movies(self, obj):
+        return ", ".join([movie.title for movie in obj.movies.all()])
+
+    get_movies.short_description = "Movies"
+
+@admin.register(ProducerModel)
+class ProducerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_movies')
+    search_fields = ('name', )
+    ordering = ('name', )
+    def get_movies(self, obj):
+        return ", ".join([movie.title for movie in obj.movies.all()])
+
+    get_movies.short_description = "Movies"
